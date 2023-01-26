f6cd573496169a24efc98b660391a1e441348170
JamJarLid <espen.lever@gmail.com>
2023-01-26 17:52:42 +0100

    imported menu into App Component and added useStates

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,22 @@ import {
   BrowserRouter,
   Routes,
   Route
-} from 'react-router-dom'
+} from 'react-router-dom';
+import Menu from './Menu.jsx';
 
 export default function App() {
+
+  const s = useStates('main', {
+    menu: [
+//    insert menu objects here
+    ]
+  });
+
 
   return <BrowserRouter>
     <header>
       <h1>Ironboy films</h1>
-      {/* <Menu/> */}
+      <Menu/>
     </header>
   </BrowserRouter>;
 }
