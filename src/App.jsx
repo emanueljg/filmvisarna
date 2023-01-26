64bf47bfcf3046e67243d02dabce990eb4a1cd6a
emanueljg <emanueljohnsongodin@gmail.com>
2023-01-26 18:42:46 +0100

    add menu entries html

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,14 @@ export default function App() {
 
   const s = useStates('main', {
     menu: [
-//    insert menu objects here
+        // no components are here yet so I'm setting them
+        // as null as placeholders
+        { label: 'Start page', path: '/', Component: null },
+        { label: 'About', path: '/tickets', Components: null },
+        { label: 'Theatres', path: '/theatres', Components: null },
+        { label: 'Snacks', path: '/snacks', Components: null },
     ]
   });
-
 
   return <BrowserRouter>
     <header>
