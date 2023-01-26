20e8536374df6a0a8e4d17a09c09666b6153d8ca
JamJarLid <113990112+JamJarLid@users.noreply.github.com>
2023-01-26 18:20:42 +0100

    Feature header (#1)
    Add header and logo with styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 // useful hooks, comment these in, when needed:
-// import { useStates } from './utilities/states.js';
+import { useStates } from './utilities/states.js';
 // import { useEffect } from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
+import Menu from './Menu.jsx';
 
 export default function App() {
 
-  return <>
-    <h1>Hello world!</h1>
-  </>;
+  const s = useStates('main', {
+    menu: [
+//    insert menu objects here
+    ]
+  });
+
+
+  return <BrowserRouter>
+    <header>
+      <h1>Ironboy films</h1>
+      <Menu/>
+    </header>
+  </BrowserRouter>;
 }
