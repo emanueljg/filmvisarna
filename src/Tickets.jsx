f3678c0b220b358e7803aba6cfe097b873b44271
QAsRevenge <hampus.e.molin@gmail.com>
2023-01-30 13:31:58 +0100

    Changed footer color to match background

--- src/Tickets.jsx
+++ src/Tickets.jsx
@@ -6,24 +6,7 @@ export default function Tickets() {
   const s = useStates('main')
   let movies = s.movies
   let sortedMovies = s.sortedMovies;
-  //function sortMovies() {
-  //var sortTheMovies = s.movies
-  // sortTheMovies.sort(function (a, b) {
-  //  return a.title[0].localeCompare(b.title[0]);
-  //  });
 
-  // var sortedMovies = {};
-  // for (var i = 0; i < sortMovies.length; i++) {
-  //   var m = sortTheMovies[i].title[0].toUpperCase();
-  //   if (sortedMovies[m] && sortedMovies[m].length >= 0)
-  //    sortedMovies[m].push(sortTheMovies[i]);
-  //  else {
-  //    sortedMovies[m] = [];
-  //    sortedMovies[m].push(sortTheMovies[i]);
-  //  }
-  //  console.log(sortedMovies);
-  //}
-  //}
   useEffect(() => {
     // import: conditions so we don't get an endless loop to useEffects
     if (movies.length === 0 || Object.keys(s.sortedMovies).length > 0) { return; }
@@ -36,12 +19,6 @@ export default function Tickets() {
       let m = sorted[i].title[0].toUpperCase();
       byAlpha[m] = byAlpha[m] || [];
       byAlpha[m].push(sorted[i]);
-      /*if (sortedMovies[m] && sortedMovies[m].length >= 0)
-        sortedMovies[m].push(movies[i]);
-      else {
-        sortedMovies[m] = [];
-        sortedMovies[m].push(movies[i]);
-      }*/
     }
     s.sortedMovies = byAlpha;
   }, []);
