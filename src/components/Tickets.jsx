22ae736b0ce895c8e56a11efc95a3222b1befc5f
QAsRevenge <hampus.e.molin@gmail.com>
2023-02-14 10:28:25 +0100

    Filters and movie wrappers styling

--- src/components/Tickets.jsx
+++ src/components/Tickets.jsx
@@ -6,8 +6,6 @@ import '../css/tickets.css'
 
 
 export default function Tickets() {
-  const xmas95 = new Date("December 25, 1995 23:15:30");
-  const options = { weekday: "long" };
   const current = new Date();
   const date = `${current.getDate()}/${current.getMonth() + 1} - ${" " + ('idag')}`;
 
@@ -19,16 +17,7 @@ export default function Tickets() {
   const l = useStates({
     chosenCategory: 'Alla genrer',
     possibleSorts: ['Sort by name', 'Sort by length'],
-    chosenSort: 'Sort by name',
-    sortDone: '',
     chosenViewing: todayISO,
-    // note: copying the movies array from main
-    // - slice() copies an array
-    // this means when we sort the copy in the local state
-    // of this compoenent, and this does not
-    // trigger a re-mount of the component
-    // (which we otherwise would happen if changing
-    // a higer level state variable)
     movies: Array.prototype.slice.call(s.sortedMovies),
   });
 
@@ -61,28 +50,7 @@ export default function Tickets() {
     return false;
   }
 
-  
-
-  function sortMoviesAlphabetically() {
-    if (l.movies.length === 0 || s.sortedMovies.length > 0) {
-      return;
-    }
-    let sorted = l.movies.slice().sort(function (a, b) {
-      return a.title[0].localeCompare(b.title[0])
-    });
-    let byAlpha = []
-    for (let i = 0; i < sorted.length; i++) {
-      let m = sorted[i].title.toUpperCase();
-      byAlpha[m] = byAlpha[m] || []
-      byAlpha[m].push(sorted[i])
-    }
-    s.sortedMovies = byAlpha;
-  }
-
-
-
   useEffect(() => {
-    // important: conditions so we don't get an endless loop to useEffects
   }, []);
 
   let currentLetter = ""
@@ -92,7 +60,7 @@ export default function Tickets() {
         <li className="filterDay">
           <span className="pickDay">Välj dag</span>
           <section className="selectDay">
-            <select {...l.bind('chosenViewing')}>
+            <select className="chosenViewing"{...l.bind('chosenViewing')}>
               <option value={todayISO}>{date}</option>
               {s.showing.map(viewings => <option>
                 {viewings.start_date.slice(0,10)}
@@ -103,7 +71,7 @@ export default function Tickets() {
         <li className="filterGenre">
           <span className="pickGenre">Välj genre</span>
           <section className="selectGenre">
-            <select {...l.bind('chosenCategory')}>
+            <select className="chosenCategory" {...l.bind('chosenCategory')}>
               <option>Alla genrer</option>
               {s.categories.map(category => <option>
                 {category}
@@ -114,9 +82,13 @@ export default function Tickets() {
         <li className="filterFilm">
           <span className="pickFilm">Sök</span>
           <form className="selectFilm">
-            <input type="search" value={queryState.query} onChange={handleSearch}placeholder="Sök.." />
+            <input className="searchBar"type="search" value={queryState.query} onChange={handleSearch}placeholder="Sök.." />
           </form>
-          <ul>{(queryState.query === "" ? '' : queryState.listOfMovies.map(movie => { return <li className="search" >{movie.title}</li> }))}
+          <ul>{(queryState.query === "" ? '' : queryState.listOfMovies.map(movie => {
+            return <li className="search">
+              
+              {movie.title}</li>
+          }))}
           </ul>
         </li>
       </ul>
@@ -128,9 +100,9 @@ export default function Tickets() {
              <section className="letter">
               <h3>{currentLetter}</h3>
             </section>
+            <Link to={path} style={{ textDecoration: 'none' }}>
             <section className="movieInfo clear-fix">
-              <Link to={path} style={{ textDecoration: 'none' }}>
-
+              <section className="movies">
                 <img className="poster-50-percent" src={'/images/' + images[0]} />
 
                 <section className="movieText">
@@ -140,9 +112,10 @@ export default function Tickets() {
                     <span className="length">{length}</span>
                     <span className="rated">{rated}</span>
                   </h4>
+                  </section>
                 </section>
-              </Link>
-            </section>
+              </section>
+    </Link>
           </section>)
         }
 
