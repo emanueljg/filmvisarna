489ac25e067bddd73e89187c47df2e0489935085
Svenpaj <114157351+Svenpaj@users.noreply.github.com>
2023-02-15 21:51:35 +0100

    first row of the slider

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,7 @@
 import { useStates } from '../utilities/states';
 import { Link } from 'react-router-dom';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 
 export default function Home() {
@@ -7,6 +9,25 @@ export default function Home() {
   // Connect to the main state so we can read the movies
   // (that have been fetched an put in the state in the App component)
   const s = useStates('main');
+  const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 4
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1
+  }
+};
 
   return <>
  
@@ -17,22 +38,35 @@ export default function Home() {
       <div className="featuredMovieCTA-image">
         <a href="/movie/the-batman" style={{textDecoration: 'none'}}>
         <img src="/images/batman-CTA.png" alt="batat" />
-        <h1> The Batman (2022)</h1>
+          <h1> The Batman (2022)</h1>
+          <span className='feature-movie-span'>176min</span>
+          <span className='feature-movie-span-rating'>PG-13</span>
         </a>
       </div>
     </div>
 
     <div className="body-movies">
       <div className="showingNow">
-      <h1>På bio nu!</h1>
-      {s.movies.slice(0,7).map(({ path, images, title, length, rated }) =>
+        <h1>På bio nu!</h1>
+        <Carousel responsive={responsive}>
+          {s.movies.map(({ path, images, title, length, rated }) =>
         <Link to={path} className="movie">
-          <img src={'/images/' + images[0]} />
+          <img className='movie-image' src={'/images/' + images[0]} />
           <h2>{title}</h2>
-          <p>{length}</p>
+          <p className='length'>{length}</p>
           <p className='rating'>{rated}</p>
+          <p>
+            <button>Mer info</button>
+          </p>
         </Link>
-        )}
+          )}
+          
+        </Carousel>;
+
+
+
+
+      
       </div>
     </div>
   </>;
