902a4da109d381ebd512fbdf2d1a07fea387dad7
emanueljg <emanueljohnsongodin@gmail.com>
2023-02-16 22:09:57 +0100

    make api/ point to backend url

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,9 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:5000',
+        target: 'https://emanueljg.com',
+        // TODO fråga thomas mer exakt vad dessa gör
+        changeOrigin: true,
         secure: false
       }
     }
