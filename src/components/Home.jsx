a1055cf4d5dda65e1791ef675677e5cf558317e5
unit01 <hanway.tran@gmail.com>
2023-02-19 18:14:54 +0100

    återställt coming soon i home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,29 +9,39 @@ export default function Home() {
   const s = useStates('main');
 
   return <>
- 
-    <div className="featuredMovie"> <img src="/images/batman_background.jpg" alt="bat" />      
+
+    <div className="featuredMovie"> <img src="/images/batman_background.jpg" alt="bat" />
       <div className="gradientFeaturedMovie"> </div>
     </div>
     <div className="featuredMovieCTA-container">
       <div className="featuredMovieCTA-image">
-        <a href="/movie/the-batman" style={{textDecoration: 'none'}}>
-        <img src="/images/batman-CTA.png" alt="batat" />
-        <h1> The Batman (2022)</h1>
+        <a href="/movie/the-batman" style={{ textDecoration: 'none' }}>
+          <img src="/images/batman-CTA.png" alt="batat" />
+          <h1> The Batman (2022)</h1>
         </a>
       </div>
     </div>
 
     <div className="body-movies">
       <div className="showingNow">
-      <h1>På bio nu!</h1>
-      {s.movies.map(({ path, images, title, length }) =>
+        <h1>På bio nu!</h1>
+        {s.movies.map(({ path, images, title, length }) =>
+          <Link to={path} className="movie">
+            <img src={'/images/' + images[0]} />
+            <h3>{title}</h3>
+          </Link>
+        )}
+      </div>
+    </div>
+    <div className="comingSoon">
+      <h1>Kommande</h1>
+      {s.movies.slice(6).map(({ path, images, title, length }) =>
         <Link to={path} className="movie">
           <img src={'/images/' + images[0]} />
           <h3>{title}</h3>
+          <h4>{length}</h4>
         </Link>
-        )}
-      </div>
+      )}
     </div>
   </>;
 }
