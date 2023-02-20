035767d50b671b857c1929434391e12d8d31ade2
Svenpaj <114157351+Svenpaj@users.noreply.github.com>
2023-02-20 22:44:21 +0100

    trailer hide and show opacity background etc

--- src/components/MovieDetail.jsx
+++ src/components/MovieDetail.jsx
@@ -38,8 +38,16 @@ export default function MovieDetail() {
     setOpen(!open);
   };
   const [visable, setVisable] = React.useState(false);
-  const handleVisable = event => { setVisable(current => !current) };
+  const handleVisable = (event) => {
+    setVisable((current) => !current);
+  };
+  const [opacity, setOpacity] = useState(1);
 
+  const opts = {
+    playerVars: {
+      autoPlay: 1,
+    },
+  };
 
   function dateRead(date_str) {
     return new Date(date_str).toLocaleString("sv-SE");
@@ -47,65 +55,100 @@ export default function MovieDetail() {
 
   return !movie ? null : (
     <section className="main">
-      <section className="container">
-        <section className="movie-background" style={{ background: `url(${"/images/" + movie.background[0]})  center top / cover no-repeat` }} >
-          {visable && (<div className="trailer-container"><YouTube className="detail-trailer" videoId={movie.trailer[0]} /></div>)}
-        </section>
+      {visable && (
+        <div className="trailer-container">
+          <YouTube
+            className="detail-trailer"
+            videoId={movie.trailer[0]}
+            opts={opts}
+          />
+          <button
+            className="close-button"
+            onClick={() => {
+              handleVisable();
+              setOpacity(1);
+            }}
+          >
+            X
+          </button>
+        </div>
+      )}
+      <section className="container" style={{ opacity }}>
+        <section
+          className="movie-background"
+          style={{
+            background: `url(${
+              "/images/" + movie.background[0]
+            })  center top / cover no-repeat`,
+          }}
+        ></section>
         <section className="button-container">
-          <button className="trailer-button" onClick={handleVisable}>{">"}</button>
+          <button
+            className="trailer-button"
+            onClick={() => {
+              handleVisable();
+              setOpacity(0.2);
+            }}
+          >
+            {">"}
+          </button>
         </section>
-        <section className="gradient">
-        </section>
+        <section className="gradient"></section>
         <section className="asd">
           <section className="title-wrapper">
             <h1 className="movie-title">{movie.title}</h1>
           </section>
-          </section>
+        </section>
         <section className="poster-and-title">
-      <section className="movie-poster-wrapper">
-        <img className="movie-poster-image"src={"/images/" + movie.images[0]} />
+          <section className="movie-poster-wrapper">
+            <img
+              className="movie-poster-image"
+              src={"/images/" + movie.images[0]}
+            />
+            <section className="movie-glr-wrap">
+              <ul>
+                {movie.genre.map((genre) => (
+                  <li className="genreTag">{genre}</li>
+                ))}
+              </ul>
+              <h4 className="movie-length">{movie.length}</h4>
+              <h4 className="movie-rated">{movie.rated}</h4>
+            </section>
           </section>
-            <section className="movieDescription">
+          <section className="movieDescription">
             <p className="descriptionP">{description}</p>
             <section className="qwerty">
- {Object.entries(shownMovieAttrs).map(([key, value]) => (
-        <>
-          {
-            <>
-              <h4 className="descriptorTitle">{key}</h4>
-              <p className="descriptorP">{value instanceof Array ? value.join(", ") : value}</p>
-            </>
-          }
-        </>
-      ))}
-      
-</section>
+              {Object.entries(shownMovieAttrs).map(([key, value]) => (
+                <>
+                  {
+                    <>
+                      <h4 className="descriptorTitle">{key}</h4>
+                      <p className="descriptorP">
+                        {value instanceof Array ? value.join(", ") : value}
+                      </p>
+                    </>
+                  }
+                </>
+              ))}
+            </section>
           </section>
-
-          <section className="movie-glr-wrap">
-            <ul>
-            {movie.genre.map(genre => (
-                <li className="genreTag">{genre}</li>
-              
-            ))}
-                  </ul>
-                    <h4 className="movie-length">{movie.length}</h4>
-                    <h4 className="movie-rated">{movie.rated}</h4>
-          </section>
-
         </section>
-     
-      <section className="dagvaljare">
-        <h2 className="valj-dag">Välj tid</h2>
-        <section className="dagar">
-          {movie.viewings.map((v) => (
-            <a href={"/" + v.start_date.replace(":", "-")} className="timelink">
-              {dateRead(v.start_date)} - {dateRead(v.end_date)} (Salong {v.room}
-              )
-            </a>
-          ))}
+
+        <section className="dagvaljare">
+          <h2 className="valj-dag">Välj tid</h2>
+          <section className="dagar">
+            {movie.viewings.map((v) => (
+              <a
+                href={"/" + v.start_date.replace(":", "-")}
+                className="timelink"
+              >
+                {dateRead(v.start_date)} - {dateRead(v.end_date)} (Salong{" "}
+                {v.room})
+              </a>
+            ))}
+          </section>
         </section>
       </section>
     </section>
-  </section>);
+  );
 }
