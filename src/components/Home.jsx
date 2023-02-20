5f604288dffb1bf3f6562beadc65d7b9154c5947
QAsRevenge <hampus.e.molin@gmail.com>
2023-02-20 11:53:33 +0100

    Merge branch 'dev' into feature-details-background

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -39,39 +39,73 @@ export default function Home() {
     },
   };
 
-  return <>
-
-    <div className="featuredMovie"> <img src="/images/batman_background.jpg" alt="bat" />
-      <div className="gradientFeaturedMovie"> </div>
-    </div>
-    <div className="featuredMovieCTA-container">
-      <div className="featuredMovieCTA-image">
-        <a href="/movie/the-batman" style={{ textDecoration: 'none' }}>
-          <img src="/images/batman-CTA.png" alt="batat" />
-          <h1> The Batman (2022)</h1>
-        </a>
+  return (
+    <>
+      <div className="featuredMovie">
+        {" "}
+        <img src="/images/batman_background.jpg" alt="bat" />
+        <div className="gradientFeaturedMovie"> </div>
+      </div>
+      <div className="featuredMovieCTA-container">
+        <div className="featuredMovieCTA-image">
+          <a href="/movie/the-batman" style={{ textDecoration: "none" }}>
+            <img src="/images/batman-CTA.png" alt="batat" />
+            <h1> The Batman (2022)</h1>
+            <span className="feature-movie-span">176min</span>
+            <span className="feature-movie-span-rating">PG-13</span>
+          </a>
+        </div>
       </div>
 
-    <div className="body-movies">
-      <div className="showingNow">
-        <h1>På bio nu!</h1>
-        {s.movies.map(({ path, images, title, length }) =>
-          <Link to={path} className="movie">
-            <img src={'/images/' + images[0]} />
-            <h3>{title}</h3>
-          </Link>
-        )}
+      <div className="body-movies">
+        <div className="showingNow">
+          <h1>På bio nu!</h1>
+          <Carousel
+            keyBoardControl={true}
+            autoPlay={true}
+            autoPlaySpeed={5000}
+            transitionDuration={100}
+            infinite={true}
+            responsive={responsive}
+          >
+            {s.movies.map(({ path, images, title, length, rated }) => (
+              <Link to={path} className="movie">
+                <img className="movie-image" src={"/images/" + images[0]} />
+                <h2>{title}</h2>
+                <p className="length">{length}</p>
+                <p className="rating">{rated}</p>
+                <p>
+                  <button>Mer info</button>
+                </p>
+              </Link>
+            ))}
+          </Carousel>
+          ;
+        </div>
+
+        <div className="comingSoon">
+          <h1>Kommer snart på bio!</h1>
+          <SlideShow>
+            {[
+              ...new Array(
+                "hebWYacbdvc",
+                "ZfVYgWYaHmE",
+                "WWWDskI46Js",
+                "32RAq6JzY-w",
+                "QsudEHsuvIg",
+                "AHmCH7iB_IM",
+                "02PPMPArNEQ"
+              ),
+            ].map((x, i) => (
+              <div className="slide">
+                {" "}
+                (
+                <YouTube className="trailer" videoId={x} opts={opts} />
+              </div>
+            ))}
+          </SlideShow>
+        </div>
       </div>
-    </div>
-    <div className="comingSoon">
-      <h1>Kommande</h1>
-      {s.movies.slice(6).map(({ path, images, title, length }) =>
-        <Link to={path} className="movie">
-          <img src={'/images/' + images[0]} />
-          <h3>{title}</h3>
-          <h4>{length}</h4>
-        </Link>
-      )}
-    </div>
-  </>;
+    </>
+  );
 }
