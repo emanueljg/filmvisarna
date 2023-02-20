0c3040fa206581f534e241a781c495ddbd0a01f1
JamJarLid <espen.lever@gmail.com>
2023-02-20 10:10:21 +0100

    initial div creation

---
+++ src/components/Booking.jsx
@@ -0,0 +1,25 @@
+import { useStates } from "../utilities/states"
+import { useParams } from "react-router-dom"
+
+export default function Booking() {
+  const { bookingPath } = useParams();
+  const s = useStates('main');
+
+  const movie = s.movies.find(movie => movie.path === '/movie/' + moviePath);
+  const theatre = s.theatres.find();
+  const showing = s.showings.find();
+
+
+  return <div className="bookingPage">
+    <div className="chosenMovie">
+      <img src={'/images/' + movie.images[0]} />
+      <h2>{movie.title}</h2>
+      <p>{theatre}</p>
+      <p>{showing}</p>
+      <div className="ticketSelector">
+
+      </div>
+      
+    </div>
+  </div>
+}
